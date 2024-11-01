17603f40b154037691d85f55490e864c9d3a6e2a
Claudenes Renato Soares de Almeida <claudenes.almeida@grupoaguasdobrasil.com.br>
2024-11-01 11:37:47 -0300

    Implementacao da Camada WebUI

---
+++ Avaliacao/Avaliacao.WebUI/Controllers/ClienteController.cs
@@ -0,0 +1,102 @@
+﻿using Avaliacao.Application.Dtos;
+using Avaliacao.Application.Interfaces;
+using Avaliacao.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Avaliacao.WebUI.Controllers;
+
+public class ClienteController : Controller
+{
+    private readonly IClienteService _clienteService;
+    private readonly IEnderecoService _enderecoService;
+    private readonly IWebHostEnvironment _environment;
+
+    public ClienteController(IClienteService clienteService, IEnderecoService enderecoService, IWebHostEnvironment environment)
+    {
+        _clienteService = clienteService;
+        _enderecoService = enderecoService;
+        _environment = environment;
+
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var clientes = _clienteService.ListAll();
+        return View(await clientes);
+    }
+    [HttpGet]
+    public async Task<IActionResult> Create()
+    {
+        return View();
+    }
+    [HttpGet]
+    public async Task<IActionResult> Edit(int id)
+    {
+        if (id == null)
+            return NotFound();
+
+        var clienteDto = await _clienteService.Read(id);
+
+        if (clienteDto == null) return NotFound();
+
+        return View(clienteDto);
+    }
+
+
+    [HttpPost]
+    public async Task<IActionResult> Create(ClienteDto clienteDto)
+    {
+        if (ModelState.IsValid)
+        {
+            await _clienteService.Create(clienteDto);
+            return RedirectToAction(nameof(Index));
+        }
+        
+        return View(clienteDto);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(ClienteDto clienteDto)
+    {
+        if (ModelState.IsValid)
+        {
+            await _clienteService.Update(clienteDto);
+            return RedirectToAction(nameof(Index));
+        }
+        return View(clienteDto);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Delete(int id)
+    {
+        if (id == null)
+            return NotFound();
+
+        var livroDto = await _clienteService.Read(id);
+
+        if (livroDto == null) return NotFound();
+
+        return View(livroDto);
+    }
+
+    [HttpPost(), ActionName("Delete")]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        await _clienteService.Delete(id);
+        return RedirectToAction("Index");
+    }
+
+    public async Task<IActionResult> Details(int id)
+    {
+        if (id == null) return NotFound();
+
+        var clienteDto = await _clienteService.Read(id);
+
+        if (clienteDto == null) return NotFound();
+        var wwwroot = _environment.WebRootPath;
+
+
+        return View(clienteDto);
+    }
+}
--- Avaliacao/Avaliacao.WebUI/Program.cs
+++ Avaliacao/Avaliacao.WebUI/Program.cs
@@ -1,7 +1,13 @@
+using Avaliacao.Infra.IoC;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddServices();
 
 var app = builder.Build();
 
